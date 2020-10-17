fbea34ab5c248d46b59c0427c395055804e4a881
Jesse Ostrander <jesse.ostrander@gmail.com>
2020-10-17 15:58:43 -0400

    Convert to NAPI

--- ll_keyboard_hooks.js
+++ ll_keyboard_hooks.js
@@ -2,11 +2,12 @@
 
 if (process.platform !== 'win32') return;
 
-const hookKeyboard = require('bindings')('ll_keyboard_hooks');
+const { hookKeyboard } = require('bindings')('ll_keyboard_hooks');
 
 let hooks = {};
 
 hookKeyboard((data) => {
+  console.log(data)
   const keyCodePattern = data.toString().trim().replace(/\n/g, '');
   const keyCodeEvent = keyCodePattern.split('::')[0];
   const keyCode = keyCodePattern.split('::')[1];
